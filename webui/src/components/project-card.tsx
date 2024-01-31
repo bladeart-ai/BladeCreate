0799b8321396724a83cda125d21a3c859487f168
Shiyuan Zhu <23185484+shiyuann@users.noreply.github.com>
2024-01-30 21:28:53 -0500

    chore: clean up code

--- webui/src/components/project-card.tsx
+++ webui/src/components/project-card.tsx
@@ -1,15 +1,11 @@
-import { ProjectMetadata } from '@/gen_client'
+import { Project } from '@/gen_client'
 
-export function ProjectCard(project: ProjectMetadata) {
+export function ProjectCard(project: Project) {
   return (
-    <div className="ModelCard border-1 relative h-72 w-96 gap-2 overflow-hidden rounded-lg border shadow">
-      <div className="BottomCard absolute inset-x-0 bottom-0 flex h-auto flex-col items-center justify-end gap-1 self-stretch rounded-lg bg-neutral-600/80 px-3.5 py-1">
-        <div className="ModelName self-stretch text-base font-semibold text-white">
-          {project.name}
-        </div>
-        <div className="Badges inline-flex items-start justify-start gap-1.5 self-stretch text-white">
-          {project.update_time}
-        </div>
+    <div className="flex h-fit w-full flex-col items-center justify-end gap-1 rounded-lg bg-neutral-600/80 px-3.5 py-1">
+      <div className="self-stretch text-base font-semibold text-white">{project.name}</div>
+      <div className="inline-flex items-start justify-start gap-1.5 self-stretch text-white">
+        {project.update_time}
       </div>
     </div>
   )
